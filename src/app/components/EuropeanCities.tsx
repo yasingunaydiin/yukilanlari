f8d715fe5b414bc819c33e7ba5aae2486fb6ec33
Yasin Gunaydin <81875281+yasingunaydiin@users.noreply.github.com>
2024-10-11 19:04:37 +0300

    Added middle eastern countries and every country now has 1 capital and 2 cities

--- src/app/components/EuropeanCities.tsx
+++ src/app/components/EuropeanCities.tsx
@@ -14,7 +14,7 @@ import { useState } from 'react';
 const CitiesArray = [
   {
     id: 225,
-    states: [
+    cities: [
       {
         id: 2212,
         name: 'Adana',
@@ -424,344 +424,881 @@ const CitiesArray = [
   },
   {
     id: 1,
-    states: [
+    cities: [
       {
         id: 1,
         name: 'Tirana',
+        state_code: 'AL',
+      },
+      {
+        id: 2,
+        name: 'Shkoder',
+        state_code: 'AL',
+      },
+      {
+        id: 3,
+        name: 'Durres',
         state_code: 'AL',
       },
     ],
   },
   {
     id: 2,
-    states: [
+    cities: [
       {
-        id: 2,
+        id: 4,
         name: 'Andorra la Vella',
+        state_code: 'AD',
+      },
+      {
+        id: 5,
+        name: 'Escaldes-Engordany',
+        state_code: 'AD',
+      },
+      {
+        id: 6,
+        name: 'Encamp',
         state_code: 'AD',
       },
     ],
   },
   {
-    id: 4,
-    states: [
+    id: 3,
+    cities: [
+      {
+        id: 1,
+        name: 'Yerevan',
+        state_code: 'AM',
+      },
+      {
+        id: 2,
+        name: 'Gümrü',
+        state_code: 'AM',
+      },
       {
         id: 3,
+        name: 'Vagharshapat',
+        state_code: 'AM',
+      },
+    ],
+  },
+  {
+    id: 4,
+    cities: [
+      {
+        id: 7,
         name: 'Vienna',
+        state_code: 'AT',
+      },
+      {
+        id: 8,
+        name: 'Graz',
+        state_code: 'AT',
+      },
+      {
+        id: 9,
+        name: 'Linz',
         state_code: 'AT',
       },
     ],
   },
   {
-    id: 7,
-    states: [
+    id: 5,
+    cities: [
       {
-        id: 4,
+        id: 118,
+        name: 'Baku',
+        state_code: 'AZ',
+      },
+      {
+        id: 119,
+        name: 'Ganja',
+        state_code: 'AZ',
+      },
+      {
+        id: 120,
+        name: 'Lankaran',
+        state_code: 'AZ',
+      },
+    ],
+  },
+  {
+    id: 6,
+    cities: [
+      {
+        id: 1,
+        name: 'Minsk',
+        state_code: 'BY',
+      },
+      {
+        id: 2,
+        name: 'Brest',
+        state_code: 'BY',
+      },
+      {
+        id: 3,
+        name: 'Gomel',
+        state_code: 'BY',
+      },
+    ],
+  },
+  {
+    id: 7,
+    cities: [
+      {
+        id: 10,
         name: 'Brussels',
+        state_code: 'BE',
+      },
+      {
+        id: 11,
+        name: 'Antwerp',
+        state_code: 'BE',
+      },
+      {
+        id: 12,
+        name: 'Ghent',
         state_code: 'BE',
       },
     ],
   },
   {
-    id: 9,
-    states: [
+    id: 8,
+    cities: [
       {
-        id: 5,
+        id: 1,
+        name: 'Sarajevo',
+        state_code: 'BA',
+      },
+      {
+        id: 2,
+        name: 'Banja Luka',
+        state_code: 'BA',
+      },
+      {
+        id: 3,
+        name: 'Tuzla',
+        state_code: 'BA',
+      },
+    ],
+  },
+  {
+    id: 9,
+    cities: [
+      {
+        id: 13,
         name: 'Sofia',
+        state_code: 'BG',
+      },
+      {
+        id: 14,
+        name: 'Plovdiv',
+        state_code: 'BG',
+      },
+      {
+        id: 15,
+        name: 'Varna',
         state_code: 'BG',
       },
     ],
   },
   {
     id: 10,
-    states: [
+    cities: [
       {
-        id: 6,
+        id: 16,
         name: 'Zagreb',
+        state_code: 'HR',
+      },
+      {
+        id: 17,
+        name: 'Split',
+        state_code: 'HR',
+      },
+      {
+        id: 18,
+        name: 'Rijeka',
         state_code: 'HR',
       },
     ],
   },
   {
     id: 11,
-    states: [
+    cities: [
       {
-        id: 7,
-        name: 'Cyprus',
+        id: 19,
+        name: 'Nicosia',
         state_code: 'CY',
       },
       {
-        id: 37,
-        name: 'Nicosia',
+        id: 20,
+        name: 'Limassol',
+        state_code: 'CY',
+      },
+      {
+        id: 21,
+        name: 'Larnaca',
         state_code: 'CY',
       },
     ],
   },
   {
     id: 12,
-    states: [
+    cities: [
       {
-        id: 8,
+        id: 22,
         name: 'Prague',
+        state_code: 'CZ',
+      },
+      {
+        id: 23,
+        name: 'Brno',
+        state_code: 'CZ',
+      },
+      {
+        id: 24,
+        name: 'Ostrava',
         state_code: 'CZ',
       },
     ],
   },
   {
     id: 13,
-    states: [
+    cities: [
       {
-        id: 9,
+        id: 25,
         name: 'Copenhagen',
+        state_code: 'DK',
+      },
+      {
+        id: 26,
+        name: 'Aarhus',
+        state_code: 'DK',
+      },
+      {
+        id: 27,
+        name: 'Odense',
         state_code: 'DK',
       },
     ],
   },
   {
     id: 14,
-    states: [
+    cities: [
       {
-        id: 10,
+        id: 28,
         name: 'Tallinn',
+        state_code: 'EE',
+      },
+      {
+        id: 29,
+        name: 'Tartu',
+        state_code: 'EE',
+      },
+      {
+        id: 30,
+        name: 'Narva',
         state_code: 'EE',
       },
     ],
   },
   {
     id: 15,
-    states: [
+    cities: [
       {
-        id: 11,
+        id: 31,
         name: 'Helsinki',
+        state_code: 'FI',
+      },
+      {
+        id: 32,
+        name: 'Espoo',
+        state_code: 'FI',
+      },
+      {
+        id: 33,
+        name: 'Vantaa',
         state_code: 'FI',
       },
     ],
   },
   {
     id: 16,
-    states: [
+    cities: [
       {
-        id: 12,
+        id: 34,
         name: 'Paris',
+        state_code: 'FR',
+      },
+      {
+        id: 35,
+        name: 'Marseille',
+        state_code: 'FR',
+      },
+      {
+        id: 36,
+        name: 'Lyon',
         state_code: 'FR',
       },
     ],
   },
   {
-    id: 18,
-    states: [
+    id: 17,
+    cities: [
       {
-        id: 13,
+        id: 1,
+        name: 'Tiflis',
+        state_code: 'GE',
+      },
+      {
+        id: 2,
+        name: 'Batum',
+        state_code: 'GE',
+      },
+      {
+        id: 3,
+        name: 'Kutaisi',
+        state_code: 'GE',
+      },
+    ],
+  },
+
+  {
+    id: 18,
+    cities: [
+      {
+        id: 37,
         name: 'Berlin',
+        state_code: 'DE',
+      },
+      {
+        id: 38,
+        name: 'Munich',
+        state_code: 'DE',
+      },
+      {
+        id: 39,
+        name: 'Hamburg',
         state_code: 'DE',
       },
     ],
   },
   {
     id: 19,
-    states: [
+    cities: [
       {
-        id: 14,
+        id: 40,
         name: 'Athens',
+        state_code: 'GR',
+      },
+      {
+        id: 41,
+        name: 'Thessaloniki',
+        state_code: 'GR',
+      },
+      {
+        id: 42,
+        name: 'Patras',
         state_code: 'GR',
       },
     ],
   },
   {
     id: 20,
-    states: [
+    cities: [
       {
-        id: 15,
+        id: 43,
         name: 'Budapest',
+        state_code: 'HU',
+      },
+      {
+        id: 44,
+        name: 'Debrecen',
+        state_code: 'HU',
+      },
+      {
+        id: 45,
+        name: 'Szeged',
         state_code: 'HU',
       },
     ],
   },
   {
     id: 21,
-    states: [
+    cities: [
       {
-        id: 16,
+        id: 46,
         name: 'Reykjavik',
+        state_code: 'IS',
+      },
+      {
+        id: 47,
+        name: 'Kopavogur',
+        state_code: 'IS',
+      },
+      {
+        id: 48,
+        name: 'Hafnarfjordur',
         state_code: 'IS',
       },
     ],
   },
   {
     id: 22,
-    states: [
+    cities: [
       {
-        id: 17,
+        id: 49,
         name: 'Dublin',
+        state_code: 'IE',
+      },
+      {
+        id: 50,
+        name: 'Cork',
+        state_code: 'IE',
+      },
+      {
+        id: 51,
+        name: 'Limerick',
         state_code: 'IE',
       },
     ],
   },
   {
     id: 23,
-    states: [
+    cities: [
       {
-        id: 18,
+        id: 52,
         name: 'Rome',
+        state_code: 'IT',
+      },
+      {
+        id: 53,
+        name: 'Milan',
+        state_code: 'IT',
+      },
+      {
+        id: 54,
+        name: 'Naples',
         state_code: 'IT',
       },
     ],
   },
   {
-    id: 50,
-    states: [
+    id: 24,
+    cities: [
       {
-        id: 19,
-        name: 'Pristina',
-        state_code: 'XK',
+        id: 1,
+        name: 'Lefkoşa',
+        state_code: 'CY',
+      },
+      {
+        id: 2,
+        name: 'Gazimağusa',
+        state_code: 'CY',
+      },
+      {
+        id: 3,
+        name: 'Girne',
+        state_code: 'CY',
       },
     ],
   },
   {
     id: 25,
-    states: [
+    cities: [
       {
-        id: 20,
+        id: 58,
         name: 'Riga',
+        state_code: 'LV',
+      },
+      {
+        id: 59,
+        name: 'Daugavpils',
+        state_code: 'LV',
+      },
+      {
+        id: 60,
+        name: 'Liepaaja',
         state_code: 'LV',
       },
     ],
   },
   {
     id: 26,
-    states: [
+    cities: [
       {
-        id: 21,
+        id: 61,
         name: 'Vilnius',
+        state_code: 'LT',
+      },
+      {
+        id: 62,
+        name: 'Kaunas',
+        state_code: 'LT',
+      },
+      {
+        id: 63,
+        name: 'Klaipeda',
         state_code: 'LT',
       },
     ],
   },
   {
     id: 27,
-    states: [
+    cities: [
       {
-        id: 22,
+        id: 64,
         name: 'Luxembourg City',
+        state_code: 'LU',
+      },
+      {
+        id: 65,
+        name: 'Echternach',
+        state_code: 'LU',
+      },
+      {
+        id: 66,
+        name: 'Differdange',
         state_code: 'LU',
       },
     ],
   },
   {
     id: 28,
-    states: [
+    cities: [
       {
-        id: 23,
+        id: 1,
         name: 'Valletta',
+        state_code: 'MT',
+      },
+      {
+        id: 2,
+        name: 'Birkirkara',
+        state_code: 'MT',
+      },
+      {
+        id: 3,
+        name: 'Mosta',
+        state_code: 'MT',
+      },
+    ],
+  },
+
+  {
+    id: 29,
+    cities: [
+      {
+        id: 70,
+        name: 'Valletta',
+        state_code: 'MT',
+      },
+      {
+        id: 71,
+        name: 'Birkirkara',
+        state_code: 'MT',
+      },
+      {
+        id: 72,
+        name: 'Sliema',
         state_code: 'MT',
       },
     ],
   },
   {
-    id: 32,
-    states: [
+    id: 30,
+    cities: [
       {
-        id: 24,
+        id: 1,
+        name: 'Monako',
+        state_code: 'MC',
+      },
+      {
+        id: 2,
+        name: 'Monte Carlo',
+        state_code: 'MC',
+      },
+      {
+        id: 3,
+        name: 'La Condamine',
+        state_code: 'MC',
+      },
+    ],
+  },
+  {
+    id: 31,
+    cities: [
+      {
+        id: 1,
+        name: 'Podgorica',
+        state_code: 'ME',
+      },
+      {
+        id: 2,
+        name: 'Herceg Novi',
+        state_code: 'ME',
+      },
+      {
+        id: 3,
+        name: 'Kotor',
+        state_code: 'ME',
+      },
+    ],
+  },
+  {
+    id: 32,
+    cities: [
+      {
+        id: 76,
         name: 'Amsterdam',
+        state_code: 'NL',
+      },
+      {
+        id: 77,
+        name: 'Rotterdam',
+        state_code: 'NL',
+      },
+      {
+        id: 78,
+        name: 'Lahey',
         state_code: 'NL',
       },
     ],
   },
   {
-    id: 34,
-    states: [
+    id: 33,
+    cities: [
       {
-        id: 25,
+        id: 1,
+        name: 'Üsküp',
+        state_code: 'MK',
+      },
+      {
+        id: 2,
+        name: 'Manisa',
+        state_code: 'MK',
+      },
+      {
+        id: 3,
+        name: 'Kumanova',
+        state_code: 'MK',
+      },
+    ],
+  },
+  {
+    id: 34,
+    cities: [
+      {
+        id: 79,
         name: 'Oslo',
+        state_code: 'NO',
+      },
+      {
+        id: 80,
+        name: 'Bergen',
+        state_code: 'NO',
+      },
+      {
+        id: 81,
+        name: 'Stavanger',
         state_code: 'NO',
       },
     ],
   },
   {
-    id: 36,
-    states: [
+    id: 35,
+    cities: [
       {
-        id: 26,
+        id: 82,
+        name: 'Warsaw',
+        state_code: 'PL',
+      },
+      {
+        id: 83,
+        name: 'Kraków',
+        state_code: 'PL',
+      },
+      {
+        id: 84,
+        name: 'Wroclaw',
+        state_code: 'PL',
+      },
+    ],
+  },
+  {
+    id: 36,
+    cities: [
+      {
+        id: 85,
         name: 'Lisbon',
+        state_code: 'PT',
+      },
+      {
+        id: 86,
+        name: 'Porto',
+        state_code: 'PT',
+      },
+      {
+        id: 87,
+        name: 'Coimbra',
         state_code: 'PT',
       },
     ],
   },
   {
     id: 37,
-    states: [
+    cities: [
       {
-        id: 27,
+        id: 88,
         name: 'Bucharest',
+        state_code: 'RO',
+      },
+      {
+        id: 89,
+        name: 'Cluj-Napoca',
+        state_code: 'RO',
+      },
+      {
+        id: 90,
+        name: 'Timisoara',
         state_code: 'RO',
       },
     ],
   },
   {
     id: 38,
-    states: [
+    cities: [
       {
-        id: 28,
+        id: 91,
         name: 'Moscow',
+        state_code: 'RU',
+      },
+      {
+        id: 92,
+        name: 'Saint Petersburg',
+        state_code: 'RU',
+      },
+      {
+        id: 93,
+        name: 'Novosibirsk',
         state_code: 'RU',
       },
     ],
   },
   {
-    id: 39,
-    states: [
-      {
-        id: 29,
-        name: 'San Marino',
-        state_code: 'SM',
-      },
-    ],
-  },
-  {
     id: 40,
-    states: [
+    cities: [
       {
-        id: 30,
+        id: 94,
         name: 'Belgrade',
+        state_code: 'RS',
+      },
+      {
+        id: 95,
+        name: 'Novi Sad',
+        state_code: 'RS',
+      },
+      {
+        id: 96,
+        name: 'Niš',
         state_code: 'RS',
       },
     ],
   },
   {
     id: 41,
-    states: [
+    cities: [
       {
-        id: 31,
+        id: 97,
         name: 'Bratislava',
+        state_code: 'SK',
+      },
+      {
+        id: 98,
+        name: 'Košice',
+        state_code: 'SK',
+      },
+      {
+        id: 99,
+        name: 'Prešov',
         state_code: 'SK',
       },
     ],
   },
   {
     id: 42,
-    states: [
+    cities: [
       {
-        id: 32,
-        name: 'Ljubljana',
+        id: 1,
+        name: 'Lubljana',
+        state_code: 'SI',
+      },
+      {
+        id: 2,
+        name: 'Maribor',
+        state_code: 'SI',
+      },
+      {
+        id: 3,
+        name: 'Celje',
         state_code: 'SI',
       },
     ],
   },
   {
-    id: 44,
-    states: [
+    id: 43,
+    cities: [
       {
-        id: 33,
+        id: 1,
+        name: 'Madrid',
+        state_code: 'ES',
+      },
+      {
+        id: 2,
+        name: 'Barcelona',
+        state_code: 'ES',
+      },
+      {
+        id: 3,
+        name: 'Valensiya',
+        state_code: 'ES',
+      },
+    ],
+  },
+  {
+    id: 44,
+    cities: [
+      {
+        id: 1,
         name: 'Stockholm',
+        state_code: 'SE',
+      },
+      {
+        id: 2,
+        name: 'Göteborg',
+        state_code: 'SE',
+      },
+      {
+        id: 3,
+        name: 'Malmö',
         state_code: 'SE',
       },
     ],
   },
   {
     id: 45,
-    states: [
+    cities: [
       {
-        id: 34,
+        id: 1,
+        name: 'Zürih',
+        state_code: 'CH',
+      },
+      {
+        id: 2,
+        name: 'Cenevre',
+        state_code: 'CH',
+      },
+      {
+        id: 3,
         name: 'Bern',
         state_code: 'CH',
       },
@@ -769,22 +1306,175 @@ const CitiesArray = [
   },
   {
     id: 46,
-    states: [
+    cities: [
       {
-        id: 35,
+        id: 1,
         name: 'Kyiv',
+        state_code: 'UA',
+      },
+      {
+        id: 2,
+        name: 'Kharkiv',
+        state_code: 'UA',
+      },
+      {
+        id: 3,
+        name: 'Odesa',
         state_code: 'UA',
       },
     ],
   },
   {
     id: 47,
-    states: [
+    cities: [
       {
-        id: 36,
+        id: 1,
         name: 'London',
         state_code: 'GB',
       },
+      {
+        id: 2,
+        name: 'Edinburgh',
+        state_code: 'GB',
+      },
+      {
+        id: 3,
+        name: 'Birmingham',
+        state_code: 'GB',
+      },
+    ],
+  },
+  {
+    id: 48,
+    cities: [
+      { id: 1, name: 'Muskat', state_code: 'OM' },
+      { id: 2, name: 'Salalah', state_code: 'OM' },
+      { id: 3, name: 'Sohar', state_code: 'OM' },
+    ],
+  },
+  {
+    id: 49,
+    cities: [
+      { id: 1, name: 'Tel Aviv', state_code: 'IL' },
+      { id: 2, name: 'Haifa', state_code: 'IL' },
+      { id: 3, name: 'Nasıra', state_code: 'IL' },
+    ],
+  },
+  {
+    id: 50,
+    cities: [
+      {
+        id: 55,
+        name: 'Pristina',
+        state_code: 'XK',
+      },
+      {
+        id: 56,
+        name: 'Mitrovica',
+        state_code: 'XK',
+      },
+      {
+        id: 57,
+        name: 'Ferizaj',
+        state_code: 'XK',
+      },
+    ],
+  },
+  {
+    id: 51,
+    cities: [
+      { id: 4, name: 'Teheran', state_code: 'IR' },
+      { id: 1, name: 'Meşhed', state_code: 'IR' },
+      { id: 2, name: 'İsfahan', state_code: 'IR' },
+      { id: 3, name: 'Karaj', state_code: 'IR' },
+    ],
+  },
+  {
+    id: 52,
+    cities: [
+      { id: 1, name: 'Basra', state_code: 'IQ' },
+      { id: 2, name: 'Musul', state_code: 'IQ' },
+      { id: 3, name: 'Erbil', state_code: 'IQ' },
+      { id: 3, name: 'Bağdad', state_code: 'IQ' },
+    ],
+  },
+  {
+    id: 53,
+    cities: [
+      { id: 1, name: 'Cidde', state_code: 'SA' },
+      { id: 2, name: 'Mekke', state_code: 'SA' },
+      { id: 3, name: 'Dammam', state_code: 'SA' },
+      { id: 4, name: 'Riyad', state_code: 'SA' },
+    ],
+  },
+  {
+    id: 55,
+    cities: [
+      { id: 1, name: 'Dubai', state_code: 'AE' },
+      { id: 2, name: 'Sharjah', state_code: 'AE' },
+      { id: 3, name: 'Ajman', state_code: 'AE' },
+      { id: 4, name: 'Abu Dhabi', state_code: 'AE' },
+    ],
+  },
+  {
+    id: 56,
+    cities: [
+      { id: 1, name: 'Al Rayyan', state_code: 'QA' },
+      { id: 2, name: 'Al Wakrah', state_code: 'QA' },
+      { id: 3, name: 'Al Khor', state_code: 'QA' },
+      { id: 4, name: 'Doha', state_code: 'QA' },
+    ],
+  },
+  {
+    id: 57,
+    cities: [
+      { id: 1, name: 'Hawalli', state_code: 'KW' },
+      { id: 2, name: 'Salmiya', state_code: 'KW' },
+      { id: 3, name: 'Fahaheel', state_code: 'KW' },
+      { id: 4, name: 'Kuveyt', state_code: 'KW' },
+    ],
+  },
+  {
+    id: 58,
+    cities: [
+      { id: 1, name: 'Muharrak', state_code: 'BH' },
+      { id: 2, name: 'Riffa', state_code: 'BH' },
+      { id: 3, name: 'Sitra', state_code: 'BH' },
+      { id: 4, name: 'Manama', state_code: 'BH' },
+    ],
+  },
+  {
+    id: 59,
+    cities: [
+      { id: 1, name: 'Zerka', state_code: 'JO' },
+      { id: 2, name: 'İrbid', state_code: 'JO' },
+      { id: 3, name: 'Akabe', state_code: 'JO' },
+      { id: 4, name: 'Amman', state_code: 'JO' },
+    ],
+  },
+  {
+    id: 60,
+    cities: [
+      { id: 1, name: 'Halep', state_code: 'SY' },
+      { id: 2, name: 'Humus', state_code: 'SY' },
+      { id: 3, name: 'Lazkiye', state_code: 'SY' },
+      { id: 4, name: 'Şam', state_code: 'SY' },
+    ],
+  },
+  {
+    id: 61,
+    cities: [
+      { id: 1, name: 'Trablus', state_code: 'LB' },
+      { id: 2, name: 'Sidon', state_code: 'LB' },
+      { id: 3, name: 'Tyre', state_code: 'LB' },
+    ],
+  },
+  {
+    id: 62,
+    cities: [
+      { id: 1, name: 'Beytüllahim', state_code: 'PS' },
+      { id: 2, name: 'Hebron', state_code: 'PS' },
+      { id: 3, name: 'Cenin', state_code: 'PS' },
     ],
   },
 ];
@@ -806,7 +1496,7 @@ export default function CitySelect({
   };
 
   const cities =
-    CitiesArray.find((country) => country.id === selectedCountryId)?.states ||
+    CitiesArray.find((country) => country.id === selectedCountryId)?.cities ||
     [];
 
   return (
