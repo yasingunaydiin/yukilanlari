63198c646aac888984480a159f12199794020655
Yasin Gunaydin <81875281+yasingunaydiin@users.noreply.github.com>
2024-12-10 22:18:31 +0300

    removed mc clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,15 +35,6 @@ export default function RootLayout({
             gtag('config', 'G-CJJ5RK5220');
           `}
         </Script>
-        <Script id='clarity-script' strategy='afterInteractive'>
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
-        </Script>
         <main className='p-4 px-6 container mx-auto'>
           <Header />
           <AuthKitProvider>{children}</AuthKitProvider>
